b280ac15ede1db873885980b0141d8e769d0d998
CN045696 <CN045696@W1571417.northamerica.cerner.net>
2017-08-24 00:09:19 +0530

    commit

---
+++ src/string/stringclass.java
@@ -0,0 +1,16 @@
+package string;
+
+public class stringclass {
+
+	
+		public static void main(String[] args) {
+			
+			int i=567;
+			
+			String s= new String().valueOf(i);
+			System.out.println("String is"+s);
+			
+			
+
+		}
+		}
